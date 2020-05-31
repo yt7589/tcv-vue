399533d11e73b4e265525cd2e055b3c54b736f1f
yt7589 <yt7589@qq.com>
2020-05-31 23:19:29 +0800

    add known brands statistics function

--- src/views/brand_db/known_brands/index.vue
+++ src/views/brand_db/known_brands/index.vue
@@ -1,7 +1,39 @@
 <template>
   <div style="padding:30px;">
-    <p>已有品牌统计</p>
-    {{ msg }}
+    <p v-show="showBrands">共有{{ total }}个品牌：</p>
+    <p v-show="showProgress">加载中请稍候...</p>
+    <el-table
+      :key="name"
+      v-loading="showLoading"
+      :data="brands"
+      border
+      fit
+      highlight-current-row
+      style="width: 330px;"
+    >
+      <el-table-column label="序号" width="110px" align="center">
+        <template>
+          <span>*</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="品牌名称" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="数量" width="110px" align="center">
+        <template slot-scope="{row}">
+          <span>{{ row.num }}</span>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!--
+    <ul v-show="showBrands">
+      <li v-for="(item, idx) in brands" :key="item.name">
+        {{ idx + 1 }}. {{ item.name }}: {{ item.num }}
+      </li>
+    </ul>
+    -->
   </div>
 </template>
 
@@ -11,7 +43,11 @@ import axios from 'axios'
 export default {
   data() {
     return {
-      msg: 'Local Defaults!'
+      total: 0,
+      brands: null,
+      showProgress: true,
+      showBrands: false,
+      showLoading: true
     }
   },
   created() {
@@ -19,10 +55,15 @@ export default {
   },
   methods: {
     getMessage() {
-      const path = this.app_global.serverBase + 'getBrands'
+      const path = this.app_global.serverBase + 'admin/getKnownBrands'
+      console.log(path)
       axios.get(path)
         .then((res) => {
-          this.msg = res.data
+          this.showProgress = false
+          this.showBrands = true
+          this.showLoading = false
+          this.total = res.data.data.total
+          this.brands = res.data.data.brands
         })
         .catch((error) => {
           // eslint-disable-next-line
