31e0b8a371610ac3396b86368cac792ed4f1805a
yt7589 <yt7589@qq.com>
2020-06-11 14:10:40 +0800

    display brand-model-year info

--- src/views/data_anno/set_bmy_example/index.vue
+++ src/views/data_anno/set_bmy_example/index.vue
@@ -1,5 +1,55 @@
 <template>
   <div style="padding:30px;">
-    <p>设置示例图片</p>
+    <p>年款共{{ total }}个</p>
+    <p>
+      {{ bmys[idx].bmy_id }} &nbsp; : &nbsp; {{ bmys[idx].bmy_name }}&nbsp;
+      <button @click="prevBmy">上一款</button>&nbsp;
+      <button @click="nextBmy">下一款</button>&nbsp;
+    </p>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      idx: 0,
+      total: 0,
+      bmys: null
+    }
+  },
+  created() {
+    this.getBmys()
+  },
+  methods: {
+    getBmys() {
+      const path = this.app_global.serverBase + 'admin/getBmys'
+      console.log(path)
+      axios.get(path)
+        .then((res) => {
+          console.log('mode=1 response')
+          this.total = res.data.data.total
+          this.bmys = res.data.data.bmys
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error)
+        })
+    },
+    prevBmy() {
+      this.idx -= 1
+      if (this.idx < 0) {
+        this.idx = 0
+      }
+    },
+    nextBmy() {
+      this.idx += 1
+      if (this.idx >= this.bmys.length) {
+        this.idx = this.bmys.length - 1
+      }
+    }
+  }
+}
+</script>
