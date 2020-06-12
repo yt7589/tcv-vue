277a8167c29c79eb365299cb807114f3736e017b
yt7589 <yt7589@qq.com>
2020-06-12 14:19:53 +0800

    add generate delta dataset web page

--- src/views/data_manager/generate_delta_ds/index.vue
+++ src/views/data_manager/generate_delta_ds/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div style="padding:30px;">
     <p>生成增量数据集</p>
+    <p>负责人：<input v-model="workerId" type="text"></p>
+    <button @click="createDeltaDs">生成</button>&nbsp;
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      workerId: ''
+    }
+  },
+  methods: {
+    createDeltaDs() {
+      const path = this.app_global.serverBase + 'admin/createDeltaDs?workerId=' + this.workerId
+      console.log(path)
+      axios.get(path)
+        .then((res) => {
+          console.log('get createDeltaDs response')
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    }
+  }
+}
+</script>
+
